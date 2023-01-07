fb0f9542b2303de19e7041ee2e7ae8c6e3680eeb
amanbayevv <diyoramanbayev22@gmail.com>
2023-01-07 13:16:51 +0300

    Web Example css js html

---
+++ app.js
@@ -0,0 +1,15 @@
+
+const body = document.querySelector('html')
+const conta = document.querySelector('.container')
+
+body.addEventListener('click', () => {
+    body.style.backgroundColor = 'Black'
+    conta.style.backgroundColor = 'Black'
+})
+
+
+
+
+
+
+ 
